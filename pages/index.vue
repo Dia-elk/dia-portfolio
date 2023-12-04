8153cfcffd1d194c9f134c6ab17a00e02f2fe8b5
Dia <macbook@MacBook-Pro-de-macbook.local>
2023-12-04 14:34:39 +0100

    wip

--- pages/index.vue
+++ pages/index.vue
@@ -13,6 +13,10 @@ useHead({
       name: 'description',
       content: 'Welcome to the portfolio of Dia El keantaoui, a fullstack developer proficient in Laravel, Vue.js, and Flutter. Explore my skills, projects, and learn more about my journey in the world of web and mobile development.',
     },
+    {
+      name: 'keywords',
+      content:'Dia El Keantaoui , Dia El Kantaoui, Dia Elk, Full stack developer marrakech, Laravel developer marrakech, Flutter developer marrakech'
+    },
   ],
 });
 </script>
